4d6a314264e63892e230b372eb3a49f9b638e0b0
ijkimmy <injungkim1223@gmail.com>
2022-02-27 18:11:57 +0000

    insufficient shared memory...

--- train.py
+++ train.py
@@ -320,7 +320,7 @@ if __name__ == '__main__':
     parser.add_argument('--augmentation', type=str, default='CustomAugmentation', help='data augmentation type (default: BaseAugmentation)')
     parser.add_argument("--resize", nargs="+", type=list, default=[224, 224], help='resize size for image when training')
     parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
-    parser.add_argument('--valid_batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--valid_batch_size', type=int, default=200, help='input batch size for validing (default: 1000)')
     # parser.add_argument('--k_folds', type=int, default=5, help='number of splits using k-fold (default: 5)')
     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
     parser.add_argument('--model_param', nargs='+', default='resnet false', help='model parameter (default: ResNet False)')
