81f8f88f44da07adbc0a1a4a51ebff1538996b90
ijkimmy <injungkim1223@gmail.com>
2022-03-03 02:49:54 +0000

    2022.03.02

--- run.sh
+++ run.sh
@@ -127,5 +127,53 @@
 # python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 1 --name ResNet50_Ep60_Weightv1_MASK --label mask
 # python inference.py --model PretrainedModels --model_param resnet false --output_filename output-ResNet50_Ep60_Weightv1_AGM-20220301-ijkimmmy.csv --label age gender mask --model_dir ./model/age/ResNet50_Ep60_Weightv1_AGE ./model/gender/ResNet50_Ep60_Weightv1_GENDER ./model/mask/ResNet50_Ep60_Weightv1_MASK
 
-# k fold implemented
-python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 0 --name ResNet50_Ep60_Weightv0_AGE --label age
+# k fold implemented # ResNet50_Ep60_Weightv0_AGE5
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 0 --name ResNet50_Ep60_Weightv0_AGE --label age
+# python train.py --epochs 60 --model PretrainedModels --model_param efficientnet-b3 false --weight_version 0 --name Effb3_Ep60_Weightv0_AGE2 --label age
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 0 --name ResNet50_Ep60_Weightv0_GENDER --label gender
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 0 --name ResNet50_Ep60_Weightv0_MASK --label mask
+
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 0 --name ResNet50_Ep60_Weightv0_GENDER2 --label gender
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet false --weight_version 0 --name ResNet50_Ep60_Weightv0_MASK2 --label mask
+
+# python inference.py --model PretrainedModels --model_param resnet false --output_filename output-ResNet50_kfold_Ep60_Weightv0_AGM-20220302-ijkimmmy.csv --label age gender mask --model_dir ./model/age/ResNet50_Ep60_Weightv0_AGE ./model/gender/ResNet50_Ep60_Weightv0_GENDER2 ./model/mask/ResNet50_Ep60_Weightv0_MASK2
+
+# confusion matrix & count incorrect labels in train set
+# python train.py --epochs 60 --model VGGFace --model_param true --weight_version 0 --name VGGFace_Feature_Ep60_Weightv0_AGE2 --label age
+# python train.py --epochs 60 --model VGGFace --model_param false --weight_version 0 --name VGGFace_Ep60_Weightv0_AGE2 --label age
+# test weight (weight3 >>>> weight0)
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet50 false --weight_version 3 --name ResNet50_Ep60_Weightv3_AGE --label age
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet50 false --weight_version 3 --name ResNet50_Ep60_Weightv3_GENDER --label gender
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet50 false --weight_version 3 --name ResNet50_Ep60_Weightv3_MASK --label mask
+# python inference.py --model PretrainedModels --model_param resnet false --output_filename output-ResNet50_kfold_Ep60_Weightv3_AGM-20220302-ijkimmmy.csv --label age gender mask --model_dir ./model/age/ResNet50_Ep60_Weightv3_AGE ./model/gender/ResNet50_Ep60_Weightv3_GENDER ./model/mask/ResNet50_Ep60_Weightv3_MASK
+
+# test ResNet18 vs ResNet50
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet18 false --weight_version 3 --name ResNet18_Ep60_Weightv3_AGE --label age
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet18 false --weight_version 3 --name ResNet18_Ep60_Weightv3_GENDER --label gender
+# python train.py --epochs 60 --model PretrainedModels --model_param resnet18 false --weight_version 3 --name ResNet18_Ep60_Weightv3_MASK --label mask
+# python inference.py --model PretrainedModels --model_param resnet18 false --output_filename output-ResNet18_kfold_Ep60_Weightv3_AGM-20220302-ijkimmmy-v13.csv --label age gender mask --model_dir ./model/age/ResNet18_Ep60_Weightv3_AGE ./model/gender/ResNet18_Ep60_Weightv3_GENDER ./model/mask/ResNet18_Ep60_Weightv3_MASK
+
+# python inference_copy.py --output_filename output-Voting-20220302-ijkimmy-v14.csv --model_dirs \
+# ./model/age/ResNet_Ep120_patience15_SplitProf_Downsample_CustAugv1_WeightedCEnSamplev1_SGD_AGE ./model/gender/ResNet_Ep120_patience15_SplitProf_Downsample_CustAugv1_WeightedCEnSamplev1_SGD_GENDER ./model/mask/ResNet_Ep120_patience15_SplitProf_Downsample_CustAugv1_WeightedCEnSamplev1_SGD_MASK \
+# ./model/age/ResNet50_Ep60_Weightv0_AGE ./model/gender/ResNet50_Ep60_Weightv0_GENDER2 ./model/mask/ResNet50_Ep60_Weightv0_MASK2 \
+# ./model/age/ResNet50_Ep60_Weightv3_AGE ./model/gender/ResNet50_Ep60_Weightv3_GENDER ./model/mask/ResNet50_Ep60_Weightv3_MASK \
+# ./model/age/ResNet18_Ep60_Weightv3_AGE ./model/gender/ResNet18_Ep60_Weightv3_GENDER ./model/mask/ResNet18_Ep60_Weightv3_MASK
+
+# test vggface finetune
+# python train.py --epochs 60 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep60_Weightv3_AGE --label age
+# wrong (didn't freeze layers)
+# python train.py --epochs 10 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep10_Weightv3_AGE --label age
+# python train.py --epochs 10 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep10_Weightv3_GENDER --label gender
+# python train.py --epochs 10 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep10_Weightv3_MASK --label mask
+# python inference.py --model VGGFace --model_param true --output_filename output-VGGFace_Ep10_AGM-20220303-ijkimmmy-v13.csv --label age gender mask --model_dir ./model/age/VGGFace_Feature_kf_Ep10_Weightv3_AGE ./model/gender/VGGFace_Feature_kf_Ep10_Weightv3_GENDER ./model/mask/VGGFace_Feature_kf_Ep10_Weightv3_MASK
+
+# python train.py --epochs 10 --model VGGFace --model_param false --weight_version 3 --name VGGFace_kf_Ep10_Weightv3_AGE --label age
+# python train.py --epochs 10 --model VGGFace --model_param false --weight_version 3 --name VGGFace_kf_Ep10_Weightv3_GENDER --label gender
+# python train.py --epochs 10 --model VGGFace --model_param false --weight_version 3 --name VGGFace_kf_Ep10_Weightv3_MASK --label mask
+
+python train.py --epochs 12 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep12_Weightv3_AGE --label age
+python train.py --epochs 12 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep12_Weightv3_GENDER --label gender
+python train.py --epochs 12 --model VGGFace --model_param true --weight_version 3 --name VGGFace_Feature_kf_Ep12_Weightv3_MASK --label mask
+python inference.py --model VGGFace --model_param true --output_filename output-VGGFace_Feature_kf_Ep12_AGM-20220303-ijkimmmy-v14.csv --label age gender mask --model_dir ./model/age/VGGFace_Feature_kf_Ep12_Weightv3_AGE ./model/gender/VGGFace_Feature_kf_Ep12_Weightv3_GENDER ./model/mask/VGGFace_Feature_kf_Ep12_Weightv3_MASK
+
+
