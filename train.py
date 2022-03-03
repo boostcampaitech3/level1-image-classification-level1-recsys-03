a2c4c7a92f0002d177bb0d133abbfc2362ff815b
ijkimmy <injungkim1223@gmail.com>
2022-03-03 02:49:29 +0000

    refactoring

--- train.py
+++ train.py
@@ -357,7 +357,7 @@ if __name__ == '__main__':
     parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
     parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
     parser.add_argument('--label', type=str, default='multi', help='label of the data (default: multi)')
-    parser.add_argument('--weight_version', type=int, default='0', help='implementation version of WegightedRandomSample (default: 0)')
+    parser.add_argument('--weight_version', type=int, default='3', help='implementation version of WegightedRandomSample (default: 0)')
 
     # Container environment
     parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
